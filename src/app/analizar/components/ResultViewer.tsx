628f2860d457c85b61b2917917172d69576a61d5
HiroshiAdauto <hiroshi.tomioka95@gmail.com>
2025-07-31 19:05:42 -0300

    deshabilito el componente temporalmente

--- src/app/analizar/components/ResultViewer.tsx
+++ src/app/analizar/components/ResultViewer.tsx
@@ -1,38 +1,38 @@
-"use client";
+// "use client";
 
-import {parseTransactions} from "@/app/lib/parseTransactions";
-import {Card, CardContent} from "@/components/ui/card";
-import {ScrollArea} from "@/components/ui/scroll-area";
-import {json} from "stream/consumers";
+// import {parseTransactions} from "@/app/lib/parseTransactions";
+// import {Card, CardContent} from "@/components/ui/card";
+// import {ScrollArea} from "@/components/ui/scroll-area";
+// import {json} from "stream/consumers";
 
-export default function ResultViewer({data}: any) {
-  console.log("data", data);
-  //const transactions = parseTransactions(data);
-  const transactions = data;
-  if (!transactions.length) {
-    return (
-      <p className="text-center text-sm text-muted-foreground">
-        No se encontraron movimientos.
-      </p>
-    );
-  }
+// export default function ResultViewer({data}: any) {
+//   console.log("data", data);
+//   //const transactions = parseTransactions(data);
+//   const transactions = data;
+//   if (!transactions.length) {
+//     return (
+//       <p className="text-center text-sm text-muted-foreground">
+//         No se encontraron movimientos.
+//       </p>
+//     );
+//   }
 
-  return (
-    <pre>{data}</pre>
-    // <ScrollArea className="max-h-[500px] w-full">
-    //   <div className="space-y-2">
-    //     {transactions.map((tx, idx) => (
-    //       <Card key={idx} className="rounded-2xl p-3 shadow-sm">
-    //         <CardContent className="p-0 flex flex-col gap-1">
-    //           <div className="text-sm text-muted-foreground">{tx.date}</div>
-    //           <div className="font-medium">{tx.description}</div>
-    //           <div className="text-right text-sm font-semibold">
-    //             {tx.currency} {tx.amount.toFixed(2)}
-    //           </div>
-    //         </CardContent>
-    //       </Card>
-    //     ))}
-    //   </div>
-    // </ScrollArea>
-  );
-}
+//   return (
+//     <pre>{data}</pre>
+//     // <ScrollArea className="max-h-[500px] w-full">
+//     //   <div className="space-y-2">
+//     //     {transactions.map((tx, idx) => (
+//     //       <Card key={idx} className="rounded-2xl p-3 shadow-sm">
+//     //         <CardContent className="p-0 flex flex-col gap-1">
+//     //           <div className="text-sm text-muted-foreground">{tx.date}</div>
+//     //           <div className="font-medium">{tx.description}</div>
+//     //           <div className="text-right text-sm font-semibold">
+//     //             {tx.currency} {tx.amount.toFixed(2)}
+//     //           </div>
+//     //         </CardContent>
+//     //       </Card>
+//     //     ))}
+//     //   </div>
+//     // </ScrollArea>
+//   );
+// }
